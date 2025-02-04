5e6e4fa19ef94dc8ba680463eb13cda710bce715
maelgllt <mael.guilloteau@outlook.fr>
2025-02-04 11:36:36 +0100

    push header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-
+  <header class="header">
+    <h1>Les P'tits Sorciers</h1>
+  </header>
 </template>
 
-<style scoped>
+<style>
 
+header h1 {
+  font-family: 'Dream Orphans', sans-serif;
+  font-weight: 700;
+  font-size: 45px;
+  line-height: 54px;
+  text-align: center;
+  color: #FB8B24;
+}
 </style>
